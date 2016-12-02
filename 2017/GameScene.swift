3269a5f793c6e2d0f38498cd6e446f403fb01670
Student <student@macbook-pro-894-christoffersen.local>
2016-12-02 13:21:33 -0500

    Reset to basic black background.

--- 2017/GameScene.swift
+++ 2017/GameScene.swift
@@ -1,89 +1,28 @@
-//
-//  GameScene.swift
-//  2017
-//
-//  Created by Student on 2016-11-24.
-//  Copyright © 2016 Student. All rights reserved.
-//
-
+import UIKit
 import SpriteKit
-import GameplayKit
 
-class GameScene: SKScene {
+class GameViewController : UIViewController {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func didMove(to view: SKView) {
+    override func viewDidLoad() {
+        super.viewDidLoad() // must call viewDidLoad() method on parent class (do not worry about what this means right now)
+        let scene = GameScene(size: CGSize(width: 2048, height: 1536)) // This will create our basic scene to make a game on. We need to give a size.
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        // Now we will configure the scene
+        let skView = self.view as! SKView   // Tell SpriteKit that the current view will be the one shown to user in app
+        skView.showsFPS = true              // Make sure a frames per second info text field will be shown
+        skView.showsNodeCount = true        // Keep track of how many nodes (visual elements) are on screen
+        // (The fewer nodes the better for performance reasons)
+        skView.ignoresSiblingOrder = true   // I will explain this option later.
+        scene.scaleMode = .aspectFill       // Scene will expand if necessary to fill device screen.
+        skView.presentScene(scene)          // Make the view show the scene we have configured
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
     }
     
     
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    override var prefersStatusBarHidden: Bool {
+        return true // this ensures the status bar on the device is hidden
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
 }
+
+
