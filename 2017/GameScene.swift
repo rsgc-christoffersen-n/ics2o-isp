fedae7792e0621a488e73b11c6fbe5dadd114465
Guest User <Guest@Russells-MacBook-Pro.local>
2016-12-08 10:56:01 -0500

    Changed my game view to account for somechanges I will make to my game, have some point notes on my goals in my game view.

--- 2017/GameScene.swift
+++ 2017/GameScene.swift
@@ -8,3 +8,9 @@ class GameScene : SKScene {
         
 }
     
+// Create a card structure and assign a value (0-51) to each card in the deck
+//Randomly select 26 cards for Player 1 and 26 cards for CPU and puts it into a bucket
+//Create randomizer which randomly selects cards from each deck (that connects to the button)
+//Create code which takes the card that is lost and the card that was won and puts them on the bottom of winners deck
+//When one person has no more cards left have strings apear like "game over" or "P1 wins"
+//After a few seconds have the game be able to start over
