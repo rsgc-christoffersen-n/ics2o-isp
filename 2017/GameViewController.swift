78483a85bf8b29d77edb50654726f2aedd6fc314
Guest User <Guest@Russells-MacBook-Pro.local>
2016-12-08 10:15:46 -0500

    I got my view controller set up so that it has a higher processing speed.

--- 2017/GameViewController.swift
+++ 2017/GameViewController.swift
@@ -1,58 +1,30 @@
-//
-//  GameViewController.swift
-//  2017
-//
-//  Created by Student on 2016-11-24.
-//  Copyright © 2016 Student. All rights reserved.
-//
-
 import UIKit
 import SpriteKit
-import GameplayKit
+
 
 class GameViewController: UIViewController {
-
-    @IBOutlet weak var PlayersCard: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
         
+        let scene = GameScene(size: CGSize(width: 2048, height: 1536))
+        
+        //configure of scene
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .aspectFill
+        skView.presentScene(scene)
     }
-
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    override var prefersStatusBarHidden: Bool {
+    
+    
+    override var prefersStatusBarHidden : Bool {
         return true
     }
 }
+
+
